4c948e65a272b3e771868e44d72003bebccdedf1
Finnedorn <daniele.giuntoli@outlook.com>
2023-10-23 14:57:11 +0200

    added html/css/js files

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/*
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+Trasformare la stringa foto in una immagine effettiva
+
+BONUS :
+Organizzare i singoli membri in card/schede
+Aggiungere un nuovo membro al team
+Consigli del giorno:
+Ragioniamo come sempre a step.
+Prima la logica in italiano e poi traduciamo in codice.
+*/
+
+
+
+/*
+<div class="col-4">
+    <div class="card">
+        <img class="card-img-top" src="" alt="">
+        <div class="card-body">
+            <h3 class="card-title">nome e cognome</h3>
+            <h4 class="card-text"><t-lavorativo></t-lavorativo></h4>
+        </div>
+    </div> 
+</div>
+*/
+
+
